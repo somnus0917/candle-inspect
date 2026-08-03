30d925f6e96f6624613dca558c9e19c38dea73ab
somnus from macos <somnus0917chen@hotmail.com>
2026-08-03 16:04:59 +0800

    learn:添加了前向传播

---
+++ examples/w02_linear_forward.rs
@@ -0,0 +1,33 @@
+use anyhow::{ensure, Result};
+use candle_core::{Device, Tensor};
+fn main() -> Result<()> {
+    let device = Device::Cpu;
+    let input = Tensor::from_vec(
+        vec![1.0f32, 2.0f32, 3.0f32, 4.0f32, 5.0f32, 6.0f32],
+        (2, 3),
+        &device,
+    )?;
+    let weight = Tensor::from_vec(
+        vec![
+            1.0f32, 0.0f32, 0.0f32, 0.0f32, 1.0f32, 0.0f32, 0.0f32, 0.0f32, 1.0f32, 1.0f32, 1.0f32,
+            1.0f32,
+        ],
+        (4, 3),
+        &device,
+    )?;
+    let bias = Tensor::from_vec(vec![10.0f32, 20.0f32, 30.0f32, 40.0f32], 4, &device)?;
+    let transposed_weight = weight.transpose(0, 1)?;
+    let output = input.matmul(&transposed_weight)?.broadcast_add(&bias)?;
+
+    println!("{:?}", output);
+    println!("{:?}", output.to_vec2::<f32>()?);
+
+    let expected_output = Tensor::from_vec(
+        vec![11.0f32, 22.0, 33.0, 46.0, 14.0, 25.0, 36.0, 55.0],
+        (2, 4),
+        &device,
+    )?;
+    ensure!(output.shape() == expected_output.shape());
+    ensure!(output.to_vec2::<f32>()? == expected_output.to_vec2::<f32>()?);
+    Ok(())
+}
